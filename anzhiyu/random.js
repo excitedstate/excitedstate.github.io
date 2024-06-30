76e78fdacc8d6d587dc9162734f7e61af6a09636
Kenvins <863178540@qq.com>
2024-06-30 21:30:27 +0800

    Site updated: 2024-06-30 21:30:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/27/随想/"];function toRandomPost(){
+var posts=["2024/06/27/随想/","2024/06/29/分布式系统复习/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
